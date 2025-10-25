27d336a5e4199743f9279c6fa172527ae93236b0
Umesh Joshi <uj19343@gmail.com>
2025-10-26 04:39:44 +0545

    2025/10/26

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const DB_URI = process.env.DB_URI || "";
 connectDatabase(DB_URI);
 app.get("/", (req, res) => {
-  res.send("Hello, World! joshi");
+  res.send("Hello, World! joshi umesh here");
 });
 
 app.listen(PORT, () => {
