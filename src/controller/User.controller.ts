411a26a399344693d6fdd4eab8a0e862454ab634
Umesh Joshi <uj19343@gmail.com>
2025-10-27 00:09:27 +0545

    10/26/2025

--- src/controller/User.controller.ts
+++ src/controller/User.controller.ts
@@ -48,19 +48,38 @@ export const LoginUser = async (req: Request, res: Response) => {
 
   const payLoad: IPayload = { id: user._id, email: user.email };
   const token = generateToken(payLoad);
-  res.status(200).json({
-    message: "Login successful",
-    status: "sucess",
-    statusCode: 200,
-    data: user,
-    token,
-  });
+  res
+    .cookie("access_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    })
+    .status(200)
+    .json({
+      message: "Login successful",
+      status: "sucess",
+      statusCode: 200,
+      data: user,
+      token,
+    });
 };
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await User.find();
     res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "user fetched sucessfully", user });
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
